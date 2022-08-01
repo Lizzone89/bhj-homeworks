753e3b7c3a00ccb599326b3a743b5f098d28d822
Elizaveta Ladygina <dranyinosok@gmail.com>
2022-08-01 21:26:46 +0300

    second task completed

---
+++ element-search/menu/task.js
@@ -0,0 +1,10 @@
+const menuLinks = document.querySelectorAll('.menu__link');
+
+for (let i = 0; i < menuLinks.length; i++) {
+  menuLinks[i].onclick = function () {
+    if (menuLinks[i].nextElementSibling !== null) {
+      menuLinks[i].nextElementSibling.classList.toggle('menu_active');
+      return false;
+    };
+  };
+};
