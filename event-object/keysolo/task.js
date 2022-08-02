989ede35e00882a7e167480fcf5f9d741e22506a
Elizaveta Ladygina <dranyinosok@gmail.com>
2022-08-02 20:18:32 +0300

    second task completed

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    document.addEventListener('keydown', (evt) => {
+      let symbol = this.currentSymbol.textContent;
+        if (evt.key === symbol) {
+          this.success();
+        } else {
+          this.fail();
+        }
+    });
   }
 
   success() {
